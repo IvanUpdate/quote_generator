5b1ad2ca9e2db9e4f1e3f8dbacac4d3cd73204a3
IvanUpdate <mironovvan99@gmail.com>
2020-11-16 22:35:10 +0300

    finish project

--- script.js
+++ script.js
@@ -1,15 +1,58 @@
-let apiQuotes = [];
-// Get Quote from API
-async function getQuote() {
-    //получаем цитату
-    //const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-    const apiUrl ='https://type.fit/api/quotes';
-    try {
-        const responce = await fetch(apiUrl);
-        apiQuotes = await responce.json();
-        console.log(data);
-    } catch(error){
-        console.log("oops, something goes wrong", error);
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterButton = document.getElementById('twitter-button');
+const newQuoteButton = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+//show loading element
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function complete() {
+    if (!loader.hidden) {
+        loader.hidden = true;
+        quoteContainer.hidden = false;
     }
 }
+
+// Get Quote from API
+async function getQuote() {
+    loading();
+    //get quote
+    const proxyUrl = 'https://sleepy-beach-46837.herokuapp.com/';
+    const apiUrl ='http://api.forismatic.com/api/1.0/?method=getQuote&lang=ru&format=json';
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        if (data.quoteAuthor ==='') {
+            authorText.innerText = "Неизвестный автор";
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        //Reduce font-size for long quotes
+        if (data.quoteText.length > 50){
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText;
+        complete();
+    } catch (error) {
+        getQuote();
+    }
+}
+
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+
+newQuoteButton.addEventListener('click', getQuote);
+twitterButton.addEventListener('click', tweetQuote);
+
 getQuote();
